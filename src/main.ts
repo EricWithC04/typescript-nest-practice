fa09d79bacb79dbda47ca9ead7658bbfa855b7fb
EricWithC04 <negativegame117@gmail.com>
2024-05-04 20:34:40 -0300

    feat: class 01-types

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './style.css'
+import { name, age } from './bases/01-types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Hello Charmander 4!</h1>
+    <h1>Hello my name is ${ name } and i am ${ age } years old!</h1>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   </div>
 `
