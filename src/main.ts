eef465d14537d475fed001a3315f043d0e7b894f
EricWithC04 <negativegame117@gmail.com>
2024-05-04 20:54:36 -0300

    feat: class 03-classes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import './style.css'
 // import { name, age } from './bases/01-types';
-import { bulbasaur, pokemons } from './bases/02-objects';
+// import { bulbasaur, pokemons } from './bases/02-objects';
+import { charmander } from './bases/03-classes';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Hello ${ bulbasaur.name } ${ pokemons[0].name }!</h1>
+    <h1>Hello ${ charmander.name } ${ charmander.id }!</h1>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   </div>
 `
