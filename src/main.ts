b5957fb811bd38bb46e0f83f9b37807a6166f636
EricWithC04 <negativegame117@gmail.com>
2024-05-04 21:01:30 -0300

    feat: class 04-injection

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 // import { name, age } from './bases/01-types';
 // import { bulbasaur, pokemons } from './bases/02-objects';
-import { charmander } from './bases/03-classes';
+// import { charmander } from './bases/03-classes';
+import { charmander } from './bases/04-injection';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
