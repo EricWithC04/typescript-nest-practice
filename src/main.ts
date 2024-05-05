7290c9c8c4e42cfae2c95d47628bf26efbd24251
EricWithC04 <negativegame117@gmail.com>
2024-05-04 21:08:48 -0300

    feat: class 06-decorators2s

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css'
 // import { bulbasaur, pokemons } from './bases/02-objects';
 // import { charmander } from './bases/03-classes';
 // import { charmander } from './bases/04-injection';
-import { charmander } from './bases/05-decorators';
+// import { charmander } from './bases/05-decorators';
+import { charmander } from './bases/06-decorators2'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
